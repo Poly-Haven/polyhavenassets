31f5173eff08ca8461b2c3982058241ddb3d32e6
Greg Zaal <gregzzmail@gmail.com>
2022-07-08 13:15:29 +0200

    Add basic download script

--- operators/pull_from_polyhaven.py
+++ operators/pull_from_polyhaven.py
@@ -1,5 +1,62 @@
 import bpy
+import json
+import os
+import requests
+from pathlib import Path
 from ..utils.get_asset_lib import get_asset_lib
+
+
+def get_asset_list():
+    url = "https://api.polyhaven.com/assets?t=models"
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        return (f"Error retrieving asset list, status code: {res.status_code}", None)
+
+    return (None, res.json())
+
+
+def update_asset(slug, info, lib_dir):
+    info_fp = lib_dir / slug / "info.json"
+    if not info_fp.exists():
+        download_asset(slug, info, lib_dir, info_fp)
+
+
+def download_asset(slug, info, lib_dir, info_fp):
+    url = f"https://api.polyhaven.com/files/{slug}"
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        return (f"Error retrieving file list for {slug}, status code: {res.status_code}", None)
+
+    info['files'] = res.json()
+    info_fp.parent.mkdir(parents=True, exist_ok=True)
+    with info_fp.open('w') as f:
+        f.write(json.dumps(info, indent=4))
+
+    print("Downloading", slug)
+    res = "1k"  # Download lowest resolution by default
+    blend = info['files']['blend'][res]['blend']
+    download_file(blend['url'], lib_dir / slug / f"{slug}.blend")
+    for sub_path, incl in blend['include'].items():
+        download_file(incl['url'], lib_dir / slug / sub_path)
+
+    return (None, None)
+
+
+def download_file(url, dest):
+    print("Downloading", Path(url).name)
+
+    res = requests.get(url)
+    if res.status_code != 200:
+        return f"Error retrieving {url}, status code: {res.status_code}"
+
+    dest.parent.mkdir(parents=True, exist_ok=True)
+
+    with dest.open("wb") as f:
+        f.write(res.content)
+
+    return None
 
 
 class PHA_OT_pull_from_polyhaven(bpy.types.Operator):
@@ -17,6 +74,17 @@ class PHA_OT_pull_from_polyhaven(bpy.types.Operator):
         if asset_lib is None:
             self.report({'ERROR'}, "First open Preferences > File Paths and create an asset library named \"Poly Haven\"")
             return {'CANCELLED'}
+        if not Path(asset_lib.path).exists():
+            self.report({'ERROR'}, "Asset library path not found! Please check the folder still exists")
+            return {'CANCELLED'}
 
-        self.report({'INFO'}, asset_lib.name)
+        error, assets = get_asset_list()
+        if error:
+            self.report({'ERROR'}, error)
+            return {'CANCELLED'}
+
+        for slug, asset in assets.items():
+            update_asset(slug, asset, Path(asset_lib.path))
+
+        self.report({'INFO'}, "Downloaded")
         return {'FINISHED'}
