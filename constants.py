de4bae894afa14c46c6d515045618efc2b0012ea
Greg Zaal <gregzzmail@gmail.com>
2022-07-19 09:13:39 +0200

    Keep user agent simple

--- constants.py
+++ constants.py
@@ -1,11 +1,10 @@
 import bpy
 import os
 import requests
-from . import bl_info
 
 DATA_DIR = os.path.join(os.path.abspath(os.path.join(
     bpy.utils.resource_path('USER'), '..')), 'data', 'polyhaven')
 REQ_HEADERS = requests.utils.default_headers()
 REQ_HEADERS.update({
-    'User-Agent': f"Blender {bpy.app.version_string}: {bl_info['name']} {'.'.join(str(i) for i in bl_info['version'])}"
+    'User-Agent': f"Blender: PH Assets"
 })
