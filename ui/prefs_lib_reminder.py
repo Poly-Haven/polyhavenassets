e56d565c9e0ac63e358d4f4c1576be5167e855d5
Greg Zaal <gregzzmail@gmail.com>
2022-08-22 16:55:07 +0200

    Flake8 pls.

--- ui/prefs_lib_reminder.py
+++ ui/prefs_lib_reminder.py
@@ -11,14 +11,14 @@ def prefs_lib_reminder(self, context):
         i = icons.get_icons()
         row.label(text=text, icon_value=i["exclamation-triangle"].icon_id)
 
-    for l in context.preferences.filepaths.asset_libraries:
-        if l.name == "Poly Haven":
+    for lib in context.preferences.filepaths.asset_libraries:
+        if lib.name == "Poly Haven":
             if context.preferences.is_dirty:
                 draw_warning(self, "Don't forget to save your preferences!")
             if context.preferences.active_section == "ADDONS":
                 row = self.layout.row(align=True)
                 row.alignment = "CENTER"
-                row.label(text="Asset library location: " + l.path, icon="CHECKMARK")
+                row.label(text="Asset library location: " + lib.path, icon="CHECKMARK")
             return
 
     if context.preferences.active_section == "ADDONS":
@@ -27,6 +27,4 @@ def prefs_lib_reminder(self, context):
             'No asset library named "Poly Haven", please create one in the File Paths tab on the left.',
         )
     else:
-        draw_warning(
-            self, 'Poly Haven: No asset library named "Poly Haven", please create it!'
-        )
+        draw_warning(self, 'Poly Haven: No asset library named "Poly Haven", please create it!')
