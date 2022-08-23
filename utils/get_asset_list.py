d74a772a38a1abebcb7968db6a6de8989c2a12c5
Greg Zaal <gregzzmail@gmail.com>
2022-08-23 08:59:43 +0200

    Add early access option

--- utils/get_asset_list.py
+++ utils/get_asset_list.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+from pathlib import Path
 from ..constants import REQ_HEADERS
 
 log = logging.getLogger(__name__)
@@ -7,6 +8,10 @@ log = logging.getLogger(__name__)
 
 def get_asset_list(asset_type="all"):
     url = f"https://api.polyhaven.com/assets?t={asset_type}"
+
+    early_access = (Path(__file__).parents[1] / "early_access.json").exists()
+    url += "&future=true" if early_access else ""
+
     res = requests.get(url, headers=REQ_HEADERS)
 
     if res.status_code != 200:
