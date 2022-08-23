24e87593bc3e3722028bf530c8d35a31cbcd2dc0
Greg Zaal <gregzzmail@gmail.com>
2022-08-23 11:53:18 +0200

    Cancelling during file download will drop the thread. The actual get request will still finish in the background, but from the UI it'll appear to cancel immediately.

--- utils/download_file.py
+++ utils/download_file.py
@@ -1,18 +1,40 @@
 import bpy
 import logging
 import requests
+import threading
 from pathlib import Path
+from random import random
+from time import sleep
 from ..constants import REQ_HEADERS
 
 log = logging.getLogger(__name__)
 
+RES = {}
+
+
+def get(url, key):
+    global RES
+    RES[key] = requests.get(url, headers=REQ_HEADERS)
+
 
 def download_file(url, dest):
+    key = url + str(random() * 1000000)
     log.info(f"Downloading {Path(url).name}")
 
-    res = requests.get(url, headers=REQ_HEADERS)
+    th = threading.Thread(target=get, kwargs={"url": url, "key": key})
+    th.start()
+
+    while th.is_alive() and not bpy.context.window_manager.pha_props.progress_cancel:
+        sleep(0.1)
+
     if bpy.context.window_manager.pha_props.progress_cancel:
-        return f"Cancelled {dest.name}"
+        msg = f"Cancelled {dest.name} mid-download"
+        log.error(msg)
+        return msg
+
+    th.join()
+
+    res = RES[key]
     if res.status_code != 200:
         msg = f"Error retrieving {url}, status code: {res.status_code}"
         log.error(msg)
